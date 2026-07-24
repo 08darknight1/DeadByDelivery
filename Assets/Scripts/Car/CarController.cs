e1e49a08041afa14adb489330b34efcc719828e7
08darknight1 <nathan.diogo@gmail.com>
2026-07-24 17:04:30 -0300

    Camera now follows something and car is working (not good, but it will do for now)!

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -1,11 +1,12 @@
+using System.Collections.Generic;
 using Rewired;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public float CarAcceleration;
+    public float CarAcceleration, CarTurningSpeed;
 
-    private float CurrentCarSpeed;
+    private float HorizontalMov, VerticalMov;
 
     private bool PlayerIsOnCar;
 
@@ -13,20 +14,72 @@ public class CarController : MonoBehaviour
 
     private Player RewiredPlayer;
 
+    private List<WheelCollider> WheelList = new List<WheelCollider>();
+
     void Start()
     {
         CarRigidbody = gameObject.GetComponent<Rigidbody>();
         RewiredPlayer = ReInput.players.GetPlayer(0);
+
+        var WheelParent = gameObject.transform.Find("Wheels");
+        
+        for(int x = 0; x < 4; x++)
+        {
+            WheelList.Add(WheelParent.transform.GetChild(x).GetComponent<WheelCollider>());
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (PlayerIsOnCar)
+        if(PlayerIsOnCar)
         {
-            var horizontalMov = RewiredPlayer.GetAxisRaw("MoveX") * CarAcceleration;
-            var verticalMov = RewiredPlayer.GetAxisRaw("MoveY") * CarAcceleration;
-            CarRigidbody.AddForce(new Vector3(horizontalMov, 0, verticalMov));
+            HorizontalMov = RewiredPlayer.GetAxisRaw("MoveX");
+            VerticalMov = RewiredPlayer.GetAxisRaw("MoveY");
+
+            for(int x = 0; x < 2; x++)
+            {
+                WheelList[x].steerAngle = CarTurningSpeed * HorizontalMov;
+            }
+
+            MakeWheelModelSpin();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(PlayerIsOnCar)
+        {
+            for(int x = 0; x < WheelList.Count; x++)
+            {
+                WheelList[x].motorTorque = VerticalMov * CarAcceleration;
+
+                if(VerticalMov <= 0)
+                {
+                    CarRigidbody.linearDamping = 0.5f;
+                }
+                else
+                {
+                    CarRigidbody.linearDamping = 0.1f;
+                }
+            }
+        }
+    }
+
+    private void MakeWheelModelSpin()
+    {
+        var wheelModels = gameObject.transform.Find("Model").transform.Find("WheelModels").transform;
+
+        for(int x=0; x < 4; x++)
+        {
+            var child = wheelModels.GetChild(x).transform;
+            var childEulAng = child.localEulerAngles;
+
+            if(HorizontalMov != 0 && x <= 1)
+            {
+                child.localEulerAngles = new Vector3(childEulAng.x, WheelList[x].steerAngle - childEulAng.z, childEulAng.z);
+            }
+
+            //child.Rotate(WheelList[x].rpm / 60 * 360 * Time.deltaTime, 0, 0);
         }
     }
 
---
+++ Assets/Scripts/General/CameraController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera CameraObj;
+
+    private GameObject FocusObj;
+
+    void Start()
+    {
+        CameraObj = gameObject.GetComponent<Camera>();
+        FocusObj = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        var currentPos = CameraObj.transform.position;
+        var newPos = new Vector3(FocusObj.transform.position.x, currentPos.y, FocusObj.transform.position.z);
+        CameraObj.transform.position = newPos;
+    }
+
+    public void SetCameraFocus(GameObject newFocus)
+    {
+        FocusObj = newFocus;
+    }
+}
